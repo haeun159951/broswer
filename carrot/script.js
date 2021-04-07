fc63ebac9bb7021ed41067b0d0474bd5f4bc17a7
Haeun Kim <haeunkim@Haeunkim-Macbook-Air.local>
2021-04-06 21:26:54 -0400

    bug/carrot added

--- carrot/script.js
+++ carrot/script.js
@@ -1,41 +1,20 @@
 let field = document.querySelector('.g__field');
+let play_button = document.querySelector('playBtn');
+let game_timer = document.querySelector('timer');
+let game_score = document.querySelector('score');
+
+carrotOrBug('carrot', 5);
+carrotOrBug('bug', 5);
 
 //두사이의 난수 생성하기
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-let fieldWidth = field.offsetWidth - '80px';
-let fieldHeight = field.offsetHeight - '80px';
+let fieldWidth = field.offsetWidth - '80';
+let fieldHeight = field.offsetHeight - '80';
 
-// function setCarrot() {
-//   let carrot = document.createElement('img'); //div element
-//   carrot.setAttribute('class', 'carrot'); // class = carrot
-//   carrot.setAttribute('src', 'img/carrot.png');
-//   carrot.style.position = 'absolute';
-//   carrot.style.width = '80px';
-//   carrot.style.height = '80px';
-//   const x = randomNumber(0, fieldWidth);
-//   const y = randomNumber(0, fieldHeight);
-//   carrot.style.left = `${x}px`;
-//   carrot.style.top = `${y}px`;
-//   field.appendChild(carrot);
-// }
-
-// function setBug() {
-//   let bug = document.createElement('img'); //div element
-//   bug.setAttribute('class', 'bug'); // class = carrot
-//   bug.setAttribute('src', 'img/bug.png');
-//   bug.style.position = 'absolute';
-//   bug.style.width = '80px';
-//   bug.style.height = '80px';
-//   const x = randomNumber(0, fieldWidth);
-//   const y = randomNumber(0, fieldHeight);
-//   bug.style.left = `${x}px`;
-//   bug.style.top = `${y}px`;
-//   field.appendChild(bug);
-// }
-
+// 당근 벌레 배치하기
 function carrotOrBug(className, num) {
   for (let i = 0; i < num; i++) {
     let item = document.createElement('img');
@@ -53,6 +32,5 @@ function carrotOrBug(className, num) {
   }
 }
 
-carrotOrBug('carrot', 5);
-// setCarrot();
-// setBug();
+// game play button
+play_button.addEventListener('click', () => {});
